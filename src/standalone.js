32421c70df19132112aa9dd6b43833d383202e7c
Shawn Allen <shawnbot@users.noreply.github.com>
2020-03-24 18:04:25 -0700

    fire up standalone test document

---
+++ src/standalone.js
@@ -0,0 +1,7 @@
+import 'sf-design-system/public/dist/css/all.css'
+import '../dist/formio-sfds.css'
+
+import 'formiojs/dist/formio.full.min.js'
+import FormioSFDS from './index.js'
+
+window.Formio.use(FormioSFDS)
