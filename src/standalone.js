c8acfba13e902aa0981a3a2b6621c20b428952a3
Shawn Allen <shawnbot@users.noreply.github.com>
2020-03-24 18:38:17 -0700

    do the Formio.use() thing on load

--- src/standalone.js
+++ src/standalone.js
@@ -2,4 +2,6 @@ import 'sf-design-system/public/dist/css/all.css'
 import '../dist/formio-sfds.css'
 import FormioSFDS from './index.js'
 
-window.Formio.use(FormioSFDS)
+window.addEventListener('load', () => {
+  window.Formio.use(FormioSFDS)
+})
