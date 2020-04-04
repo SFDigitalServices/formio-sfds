63e7c9179605a2f97011a58b4dff9f986e93652e
Shawn Allen <shawnbot@users.noreply.github.com>
2020-04-04 01:24:14 -0700

    move to translations.json

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,16 +1,21 @@
-import translations from './translations.tsv'
+import translations from './translations.json'
 
-const i18n = {}
-for (const { String: name, en, ...locales } of translations) {
-  for (const [locale, value] of Object.entries(locales)) {
-    if (value) {
-      if (!(locale in i18n)) i18n[locale] = {}
-      if (name) i18n[locale][name] = value
-      i18n[locale][en] = value
-    }
-  }
-}
+const i18n = Object.assign({
+  error: 'Please fix the following errors:',
+  invalid_date: '{{field}} is not a valid date',
+  invalid_email: '{{field}} must be a valid email address',
+  invalid_regex: '{{field}} does not match the required pattern',
+  mask: '{{field}} does not match the required mask',
+  max: '{{field}} cannot be greater than {{max}}',
+  maxLength: '{{field}} must be shorter than {{length}} characters',
+  min: '{{field}} cannot be less than {{min}}.',
+  minLength: '{{field}} must be longer than {{length}} characters',
+  next: 'Next',
+  pattern: '{{field}} does not match the pattern "{{pattern}}"',
+  previous: 'Previous',
+  required: '{{field}} is required'
+}, translations)
 
-console.warn('formio-sfds i18n:', i18n)
+// console.warn('formio-sfds i18n:', i18n)
 
 export default i18n
