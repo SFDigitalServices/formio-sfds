9d02d0a915da39fe8471054809e453d293e43e75
Shawn Allen <shawnbot@users.noreply.github.com>
2020-04-04 19:17:52 -0700

    add src/hooks.js

---
+++ src/hooks.js
@@ -0,0 +1,144 @@
+import interpolate from 'interpolate'
+import 'whatwg-fetch'
+
+const { fetch } = window
+
+const CONTENT_TYPE_HEADER = 'Content-Type'
+const JSON_CONTENT_TYPE = 'application/json'
+
+export default function buildHooks (specs) {
+  const hooks = {}
+  for (const [name, spec] of Object.entries(specs)) {
+    if (typeof spec === 'object') {
+      const hook = buildHook(spec)
+      if (typeof hook === 'function') {
+        console.info('Built hook for "%s":', name, spec)
+        hooks[name] = hook
+      } else {
+        console.warn('No hook built for "%s":', name, spec)
+      }
+    } else {
+      hooks[name] = spec
+    }
+  }
+  return hooks
+}
+
+function buildHook (spec) {
+  for (const [key, value] of Object.entries(spec)) {
+    if (typeof namedBuilders[key] === 'function') {
+      return namedBuilders[key](value)
+    }
+  }
+}
+
+const namedBuilders = {
+
+  /**
+   * This is a generic HTTP service validator, which takes the form.io
+   * submission data and sends it to an external service URL, then either
+   * resolves or rejects the submission depending on whether the service
+   * request succeeded.
+   *
+   * HTTP services may respond with a 200 status and include an "error" object
+   * (or "errors" array), which will be used to reject the submission.
+   * Otherwise, 200 status indicates success and the submission will proceed
+   * as-is.
+   */
+  validateWithService (options) {
+    const { url: urlTemplate, method = 'POST' } = options
+    const getServiceURL = createTemplate(urlTemplate)
+    return (submission, next) => {
+      const { data } = submission
+      const url = getServiceURL(data)
+      console.info('validating at:', url, 'with data:', data)
+      getJSON(url, data, { method })
+        .then(res => {
+          console.info('validated:', res)
+          next()
+        })
+        .catch(next)
+    }
+  },
+
+  /**
+   * This hook redirects to a URL, which may be templated with the submission data.
+   * You'll probably want to use this in conjunction with a beforeSubmit hook, e.g.
+   *
+   * {
+   *   hooks: {
+   *     beforeSubmit: {
+   *       verifyDSW: { ... }
+   *     }
+   *   },
+   *   on: {
+   *     submit: {
+   *       redirect: '/hooray?dsw={dsw}'
+   *     }
+   *   }
+   * }
+   */
+  redirect (url) {
+    const getURL = createTemplate(url)
+    return submission => {
+      const url = getURL(submission.data)
+      window.location = url
+    }
+  },
+
+  /**
+   * This is a validator for beforeSubmit hooks that checks the submitted data
+   * against known key/value pairs. You can use it with:
+   *
+   * hooks: {
+   *   values: {foo: 'bar'}
+   * }
+   *
+   * Which will prevent submissions if the form's submission data doesn't match
+   * the key/value pairs exactly, i.e. if `submission.data.foo !== 'bar'`.
+   */
+  values (values) {
+    return (submission, next) => {
+      const { data } = submission
+      const errors = []
+      for (const [key, value] of Object.entries(values)) {
+        if (data[key] !== value) {
+          const message = errorMessage(key, value, data)
+          errors.push({ message })
+        }
+      }
+      next(errors.length ? errors : null)
+    }
+
+    function errorMessage (key, value, data) {
+      return `Expected "${key}" to have value ${JSON.stringify(value)}, but got ${JSON.stringify(data[key])}`
+    }
+  }
+}
+
+/**
+ * This is a light wrapper around fetch() that sets up the request options
+ * automatically for "typical" JSON APIs. One thing to note is that the default
+ * HTTP method is POST so that data can be easily serialized. If the "method"
+ * option is "GET", then we append the data to the request URL as query string
+ * parameters.
+ */
+function getJSON (url, data, options = {}) {
+  const { method = 'POST', headers = {}, ...rest } = options
+  headers[CONTENT_TYPE_HEADER] = JSON_CONTENT_TYPE
+  const req = { method, headers, ...rest }
+  if (method === 'GET') {
+    const urlWithParams = new URL(url)
+    for (const [key, value] of Object.entries(data)) {
+      urlWithParams.searchParams.append(key, value)
+    }
+    url = urlWithParams.toString()
+  } else {
+    req.body = JSON.stringify(data)
+  }
+  return fetch(url, req).then(res => res.json())
+}
+
+function createTemplate (template) {
+  return data => interpolate(template, data)
+}
