1ff41c5a7bd129326531c32feb5531f1fbe01457
Shawn Allen <shawnbot@users.noreply.github.com>
2020-05-04 14:54:21 -0700

    simplify loadTranslations() to take a single URL

--- src/i18n/load.js
+++ src/i18n/load.js
@@ -1,15 +1,6 @@
-import { getJSON, mergeObjects } from '../utils'
+import { getJSON } from '../utils'
 
-export default function loadTranslations (urlOrUrls) {
-  let urls = urlOrUrls
-  if (!Array.isArray(urls)) {
-    urls = [urls]
-  }
-
-  const promises = urls.map(url => {
-    return getJSON(url, {}, { method: 'GET' })
-      .then(res => res.data || {})
-  })
-  return Promise.all(promises)
-    .then(objs => mergeObjects(...objs))
+export default function loadTranslations (url) {
+  return getJSON(url, {}, { method: 'GET' })
+    .then(res => res.data || {})
 }
