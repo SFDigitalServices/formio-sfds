5c5ba3428ab296a3642f38f20132d589b12e04b1
Shawn Allen <shawnbot@users.noreply.github.com>
2020-06-04 10:26:41 -0700

    forcefully apply FormBuilder defaults in builder.js

--- src/builder.js
+++ src/builder.js
@@ -4,6 +4,10 @@ delete FormioSFDS.templates
 
 const { Formio } = window
 
+// FIXME: this hack appears to be necessary to override the form builder
+// default options
+Formio.FormBuilder.options = FormioSFDS.options
+
 patch(Formio)
 Formio.use(FormioSFDS)
 
