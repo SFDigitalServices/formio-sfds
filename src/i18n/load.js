7b57def399643b0cd4135794cad37c87bf5b6afc
Shawn Allen <shawnbot@users.noreply.github.com>
2020-07-21 16:10:17 -0700

    only return translations if status is "success"

--- src/i18n/load.js
+++ src/i18n/load.js
@@ -2,5 +2,11 @@ import { getJSON } from '../utils'
 
 export default function loadTranslations (url) {
   return getJSON(url, {}, { method: 'GET' })
-    .then(res => res.data || {})
+    .then(res => {
+      if (res.status === 'success') {
+        return res.data
+      } else {
+        throw new Error(`Translation load error: ${JSON.stringify(res)}`)
+      }
+    })
 }
