206de68f481e3274659d34f66201fca3cc3f14f8
Shawn Allen <shawnbot@users.noreply.github.com>
2020-07-24 11:59:51 -0700

    add more tests: DOM patches, machine translation disabling

--- __tests__/patch.js
+++ __tests__/patch.js
@@ -2,26 +2,42 @@
 import 'regenerator-runtime'
 import loadTranslations from '../src/i18n/load'
 import patch from '../src/patch'
-import { createElement, createForm, destroyForm } from '../lib/test-helpers'
+import { createElement, createForm, destroyForm, sleep } from '../lib/test-helpers'
 import defaultTranslations from '../src/i18n'
 import 'formiojs/dist/formio.full.min.js'
 
 jest.mock('../src/i18n/load')
+
+const { Formio } = global
+const { createForm: originalCreateForm } = Formio
 
 const SENTINEL_I18N_KEY = 'derp'
 const SENTINEL_I18N_VALUE = 'DERP!'
 defaultTranslations.en[SENTINEL_I18N_KEY] = SENTINEL_I18N_VALUE
 
 describe('patch()', () => {
-  const { Formio } = window
-  const { createForm: originalCreateForm } = Formio
-
   beforeAll(() => {
     patch(Formio)
   })
 
   it('patches Formio.createForm()', () => {
     expect(Formio.createForm).not.toBe(originalCreateForm)
+  })
+
+  describe('DOM updates', () => {
+    it('adds a div.formio-sfds wrapper element', async () => {
+      const form = await createForm()
+      expect(Array.from(form.element.parentNode.classList)).toContain('formio-sfds')
+      destroyForm(form)
+    })
+
+    it('adds .d-flex.flex-column-reverse.mb-4 to the element', async () => {
+      const form = await createForm()
+      expect(Array.from(form.element.classList)).toEqual(
+        expect.arrayContaining(['d-flex', 'flex-column-reverse', 'mb-4'])
+      )
+      destroyForm(form)
+    })
   })
 
   describe('localization', () => {
@@ -52,6 +68,7 @@ describe('patch()', () => {
     })
 
     describe('"i18n" option', () => {
+      const mockUrl = 'http://my-translations.example.app'
       it('gets the default translations with no "i18n" option', async () => {
         const form = await createForm()
         expect(form.t(SENTINEL_I18N_KEY)).toEqual(SENTINEL_I18N_VALUE)
@@ -59,34 +76,49 @@ describe('patch()', () => {
       })
 
       it('fetches translations from the URL if provided a string', async () => {
-        const url = 'http://my-translations.example.app'
         loadTranslations.mockImplementationOnce(() => ({
           es: {
             hello: 'hola'
           }
         }))
-        const form = await createForm({}, { i18n: url, language: 'es' })
-        expect(loadTranslations).toHaveBeenCalledWith(url)
+        const form = await createForm({}, { i18n: mockUrl, language: 'es' })
+        expect(loadTranslations).toHaveBeenCalledWith(mockUrl)
         expect(form.t('hello')).toEqual('hola')
         destroyForm(form)
       })
 
       it('fails gracefully if translations fail to load', async () => {
-        const url = 'http://my-translations.example.app'
         loadTranslations.mockImplementationOnce(() => {
           throw new Error('eeeek')
         })
-        const form = await createForm({}, { i18n: url, language: 'es' })
-        expect(loadTranslations).toHaveBeenCalledWith(url)
+        const form = await createForm({}, { i18n: mockUrl, language: 'es' })
+        expect(loadTranslations).toHaveBeenCalledWith(mockUrl)
         destroyForm(form)
       })
 
       it('fails gracefully if translation data is malformed', async () => {
-        const url = 'http://my-translations.example.app'
         loadTranslations.mockImplementationOnce(() => 'lolwut')
-        const form = await createForm({}, { i18n: url, language: 'es' })
-        expect(loadTranslations).toHaveBeenCalledWith(url)
+        const form = await createForm({}, { i18n: mockUrl, language: 'es' })
+        expect(loadTranslations).toHaveBeenCalledWith(mockUrl)
         destroyForm(form)
+      })
+    })
+
+    describe('machine translation', () => {
+      it('disables machine translation if "googleTranslate" === false', async () => {
+        const form = await createForm({}, { googleTranslate: false })
+        expect(form.element.getAttribute('translate')).toEqual('no')
+        expect(Array.from(form.element.classList)).toContain('notranslate')
+        destroyForm(form)
+      })
+
+      it('disables machine translation on .flatpickr-calendar elements', async () => {
+        const el = createElement('div', { class: 'flatpickr-calendar' })
+        document.body.appendChild(el)
+        await sleep(100)
+        expect(el.getAttribute('translate')).toEqual('no')
+        expect(Array.from(el.classList)).toContain('notranslate')
+        el.remove()
       })
     })
   })
