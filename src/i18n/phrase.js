e298ff4862cd1dee81a243bd714338918bd00021
Shawn Allen <shawnbot@users.noreply.github.com>
2020-07-28 14:57:55 -0700

    refactor Phrase object w/CSS, getTranslationInfo(), formatKey()

--- src/i18n/phrase.js
+++ src/i18n/phrase.js
@@ -1,15 +1,77 @@
+const { I18N_SERVICE_URL } = process.env
+
+const configDefaults = {
+  prefix: '[[__',
+  suffix: '__]]',
+  autoLowercase: false,
+  debugMode: true
+}
+
 export default {
-  enable (config) {
+  css: `
+    body { padding-bottom: 300px; }
+  `,
+
+  enableEditor (config) {
+    if (window.PHRASEAPP_ENABLED) {
+      console.warn('The Phrase editor is already loaded with config:', window.PHRASEAPP_CONFIG)
+      return false
+    }
+
     window.PHRASEAPP_ENABLED = true
-    window.PHRASEAPP_CONFIG = config
+    window.PHRASEAPP_CONFIG = Object.assign({}, configDefaults, config)
+
     this.script = document.createElement('script')
     this.script.src = `https://app.phrase.com/assets/in-context-editor/2.0/app.js?${Date.now()}`
+    this.style = document.createElement('style')
+    this.style.textContent = this.css
     document.body.appendChild(this.script)
+    document.body.appendChild(this.style)
   },
-  disable () {
+
+  formatKey (keyOrKeys, options) {
+    console.warn('formatKey()', JSON.stringify(keyOrKeys))
+    const multiple = Array.isArray(keyOrKeys)
+    const fallback = multiple ? keyOrKeys[1] : ''
+    if (multiple && !fallback) {
+      return ''
+    }
+    const key = multiple ? keyOrKeys[0] : keyOrKeys
+    const { prefix, suffix } = configDefaults
+    return `${prefix}phrase_${key}${suffix}`
+  },
+
+  disableEditor () {
     window.PHRASEAPP_ENABLED = false
     window.PHRASEAPP_CONFIG = {}
-    this.script.remove()
-    delete this.script
+    if (this.script) {
+      this.script.remove()
+      this.style.remove()
+      delete this.script
+      delete this.style
+    }
+  },
+
+  getTranslationInfo (form, serviceUrl = I18N_SERVICE_URL) {
+    const props = form.form.properties || {}
+    const {
+      phraseProjectId,
+      phraseProjectVersion,
+      i18nServiceUrl = form.options.i18nServiceUrl || serviceUrl
+    } = props
+
+    if (phraseProjectId) {
+      let url = `${i18nServiceUrl}/phrase/${phraseProjectId}`
+      if (phraseProjectVersion) {
+        url = `${url}/${phraseProjectVersion}`
+      }
+      return {
+        projectId: phraseProjectId,
+        version: phraseProjectVersion,
+        url
+      }
+    }
+
+    return undefined
   }
 }
