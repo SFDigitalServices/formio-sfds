55bfcc675ab84bb9c25b9947ef29e71e2ad35950
Shawn Allen <shawnbot@users.noreply.github.com>
2020-07-29 16:10:14 -0700

    add lots of tests for Phrase loading

--- __tests__/phrase.js
+++ __tests__/phrase.js
@@ -6,18 +6,18 @@ import patch from '../src/patch'
 import { createForm, destroyForm } from '../lib/test-helpers'
 import 'formiojs/dist/formio.full.min.js'
 
+require('dotenv').config()
+
 jest.mock('../src/i18n/load')
 
 const { Formio } = window
-const {
-  I18N_SERVICE_URL = 'https://mock-i18n-service.app'
-} = process.env
+const { I18N_SERVICE_URL } = process.env
 
 beforeAll(() => {
   patch(Formio)
 })
 
-describe('Phrase integration', () => {
+describe('Phrase helpers', () => {
   describe('Phrase.getTranslationInfo()', () => {
     it('finds form.properties.phraseProjectId', () => {
       expect(Phrase.getTranslationInfo({
@@ -29,8 +29,48 @@ describe('Phrase integration', () => {
         }
       }).projectId).toEqual(123)
     })
-  })
 
+    it('respects form.properties.i18nServiceUrl', () => {
+      const i18nServiceUrl = 'https://my-service.app'
+      expect(Phrase.getTranslationInfo({
+        options: {},
+        form: {
+          properties: {
+            phraseProjectId: 123,
+            i18nServiceUrl
+          }
+        }
+      }).url).toEqual(`${i18nServiceUrl}/phrase/123`)
+    })
+
+    it('appends form.properties.phraseProjectVersion', () => {
+      expect(Phrase.getTranslationInfo({
+        options: {},
+        form: {
+          properties: {
+            phraseProjectId: 123,
+            phraseProjectVersion: '0.0.1'
+          }
+        }
+      }).url).toEqual(`${I18N_SERVICE_URL}/phrase/123@0.0.1`)
+    })
+
+    it('interpolates form.properties.phraseProjectId', () => {
+      const i18nServiceUrl = 'https://my-service.app/translate?projectId={phraseProjectId}'
+      expect(Phrase.getTranslationInfo({
+        options: {},
+        form: {
+          properties: {
+            phraseProjectId: 123,
+            i18nServiceUrl
+          }
+        }
+      }).url).toEqual(i18nServiceUrl.replace('{phraseProjectId}', 123))
+    })
+  })
+})
+
+describe('Phrase functionality', () => {
   describe('Formio.createForm() patches', () => {
     describe('Loading translations', () => {
       it('fetches translations from the I18N_SERVICE_URL if provided a string', async () => {
@@ -47,9 +87,99 @@ describe('Phrase integration', () => {
         }, {
           language: 'es'
         })
-        expect(form.form.properties).toEqual({ phraseProjectId })
         expect(loadTranslations).toHaveBeenCalledWith(
           `${I18N_SERVICE_URL}/phrase/${phraseProjectId}`
+        )
+        expect(form.t('hello')).toEqual('hola')
+        destroyForm(form)
+      })
+
+      it('obeys properties.i18nServiceUrl if provided', async () => {
+        const phraseProjectId = 'abcd1234'
+        const i18nServiceUrl = 'https://derp.app'
+        loadTranslations.mockImplementationOnce(() => ({
+          es: {
+            hello: 'hola'
+          }
+        }))
+        const form = await createForm({
+          properties: {
+            phraseProjectId,
+            i18nServiceUrl
+          }
+        }, {
+          language: 'es'
+        })
+        expect(loadTranslations).toHaveBeenCalledWith(
+          `${i18nServiceUrl}/phrase/${phraseProjectId}`
+        )
+        expect(form.t('hello')).toEqual('hola')
+        destroyForm(form)
+      })
+
+      it('appends properties.phraseProjectVersion to the translation URL', async () => {
+        const phraseProjectId = 'abcd1234'
+        const phraseProjectVersion = '1.0.1'
+        loadTranslations.mockImplementationOnce(() => ({
+          es: {
+            hello: 'hola'
+          }
+        }))
+        const form = await createForm({
+          properties: {
+            phraseProjectId,
+            phraseProjectVersion
+          }
+        }, {
+          language: 'es'
+        })
+        expect(loadTranslations).toHaveBeenCalledWith(
+          `${I18N_SERVICE_URL}/phrase/${phraseProjectId}@${phraseProjectVersion}`
+        )
+        expect(form.t('hello')).toEqual('hola')
+        destroyForm(form)
+      })
+
+      it('uses properites.i18nServiceUrl if provided', async () => {
+        const phraseProjectId = 'abcd1234'
+        const phraseProjectVersion = '1.0.1'
+        loadTranslations.mockImplementationOnce(() => ({
+          es: {
+            hello: 'hola'
+          }
+        }))
+        const form = await createForm({
+          properties: {
+            phraseProjectId,
+            phraseProjectVersion
+          }
+        }, {
+          language: 'es'
+        })
+        expect(loadTranslations).toHaveBeenCalledWith(
+          `${I18N_SERVICE_URL}/phrase/${phraseProjectId}@${phraseProjectVersion}`
+        )
+        expect(form.t('hello')).toEqual('hola')
+        destroyForm(form)
+      })
+
+      it('can interpolate other properties into i18nServiceUrl', async () => {
+        const properties = {
+          phraseProjectId: 'abc123',
+          i18nServiceUrl: 'https://my-service.app/phrase/{phraseProjectId}'
+        }
+        loadTranslations.mockImplementationOnce(() => ({
+          es: {
+            hello: 'hola'
+          }
+        }))
+        const form = await createForm({
+          properties
+        }, {
+          language: 'es'
+        })
+        expect(loadTranslations).toHaveBeenCalledWith(
+          properties.i18nServiceUrl.replace('{phraseProjectId}', properties.phraseProjectId)
         )
         expect(form.t('hello')).toEqual('hola')
         destroyForm(form)
