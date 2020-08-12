689194c3ff1d3e749875a6352c183e22f6ae378f
Shawn Allen <shawnbot@users.noreply.github.com>
2020-08-12 14:18:25 -0700

    bring back the address component

---
+++ src/components/address.js
@@ -0,0 +1,85 @@
+const { container: Container } = window.Formio.Components.components
+
+const defaultSchema = {
+  label: 'Address',
+  hideLabel: false,
+  // XXX this is necessary to activate the container builder UI
+  type: 'container',
+  key: 'address',
+  components: [
+    {
+      label: 'Address line 1',
+      key: 'line1',
+      type: 'textfield',
+      input: true,
+      validate: { required: true },
+      customClass: 'mb-1'
+    },
+    {
+      label: 'Address line 2',
+      key: 'line2',
+      type: 'textfield',
+      input: true,
+      customClass: 'mb-1'
+    },
+    {
+      label: 'City',
+      key: 'city',
+      type: 'textfield',
+      validate: { required: true },
+      defaultValue: 'San Francisco',
+      customClass: 'mb-1'
+    },
+    {
+      type: 'columns',
+      customClass: 'mb-0',
+      columns: [
+        {
+          width: 6,
+          components: [
+            {
+              label: 'State',
+              key: 'state',
+              type: 'state',
+              customClass: 'mb-0',
+              input: true,
+              defaultValue: 'CA',
+              validate: { required: true }
+            }
+          ]
+        },
+        {
+          width: 6,
+          components: [
+            {
+              label: 'ZIP code',
+              key: 'zip',
+              type: 'zip',
+              customClass: 'mb-0',
+              input: true
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}
+
+export default class AddressComponent extends Container {
+  static schema (...extend) {
+    return Container.schema(defaultSchema, ...extend)
+  }
+
+  get defaultSchema () {
+    return defaultSchema
+  }
+
+  static get builderInfo () {
+    return {
+      title: 'Address',
+      icon: 'home',
+      group: 'sfgov',
+      schema: defaultSchema
+    }
+  }
+}
