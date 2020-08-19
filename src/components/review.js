f04bc2317afcb88e08f2ea5ebcca241357e63313
Shawn Allen <shawnbot@users.noreply.github.com>
2020-08-19 14:04:22 -0700

    npx standard --fix

--- src/components/review.js
+++ src/components/review.js
@@ -1,6 +1,6 @@
 const { component: Components } = window.Formio.Components.components
 
-const skipComponents = ["htmlelement", "review"];
+const skipComponents = ['htmlelement', 'review']
 
 export default class Review extends Components {
   static schema (...extend) {
@@ -10,9 +10,9 @@ export default class Review extends Components {
       key: 'review',
       hideLabel: true,
       tableView: true,
-	  persistent: false,
+      persistent: false
     }, ...extend)
-  }  
+  }
 
   get defaultSchema () {
     return Review.schema()
@@ -21,30 +21,30 @@ export default class Review extends Components {
   get templateName () {
     return 'review'
   }
-  
-  render(children) {
-	const components = [];
-	const { submission } = this.root.form;
-	this.root.everyComponent((comp) => {
-	  if (!skipComponents.includes(comp.type)) {
-		components.push(comp);
-	  }
-	});
-	//remove the first and last pages, the intro and review pages respectively
-	if (components[0].type == "components" && components[0].component.hideLabel == false) components.shift();
-	if (components[components.length - 1].type == "components" && components[components.length - 1].component.hideLabel == false) components.pop();	
-	return this.renderTemplate("review", {
-	  components,
-	  submission
-	});
+
+  render (children) {
+    const components = []
+    const { submission } = this.root.form
+    this.root.everyComponent((comp) => {
+      if (!skipComponents.includes(comp.type)) {
+        components.push(comp)
+      }
+    })
+    // remove the first and last pages, the intro and review pages respectively
+    if (components[0].type === 'components' && components[0].component.hideLabel === false) components.shift()
+    if (components[components.length - 1].type === 'components' && components[components.length - 1].component.hideLabel === false) components.pop()
+    return this.renderTemplate('review', {
+      components,
+      submission
+    })
   }
-  
-  attach(element) {
-	  this.loadRefs(element, {
-		  editLinks: 'multiple',
-	  })
-      this.refs.editLinks.forEach(input => {
-        this.addEventListener(input, 'click', () => this.root.setPage(this.root.getPageIndexByKey(input.dataset.key)))
-      });	  
+
+  attach (element) {
+    this.loadRefs(element, {
+      editLinks: 'multiple'
+    })
+    this.refs.editLinks.forEach(input => {
+      this.addEventListener(input, 'click', () => this.root.setPage(this.root.getPageIndexByKey(input.dataset.key)))
+    })
   }
 }
