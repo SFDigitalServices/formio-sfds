d4e941c87b5d55b095d8cd2a6abb555495c27891
Shawn Allen <shawnbot@users.noreply.github.com>
2020-08-20 11:55:51 -0700

    tidy up example.js

--- src/example.js
+++ src/example.js
@@ -4,29 +4,16 @@ const root = document.getElementById('formio')
 
 const params = new URLSearchParams(location.search || location.hash.substr(1))
 const language = params.get('lang')
-const i18n = tryParse(params.get('i18n'))
-const renderMode = params.get('mode')
-const hooks = tryParse(params.get('hooks'))
 const formData = tryParse(root.getAttribute('data-form')) || {}
-const unlockNavigation = true
 
 const options = Object.assign({
   language,
-  renderMode,
-  hooks,
-  unlockNavigation,
-  i18n,
-  googleTranslate: params.get('googleTranslate') === 'true',
-  prefill: params,
-  properties: tryParse(params.get('properties')) || {
-    backURL: 'https://sf.gov/apply-building-permit',
-    backTitle: 'Apply for a building permit'
-  }
+  unlockNavigation: true,
+  googleTranslate: params.get('googleTranslate') === 'true'
 }, tryParse(root.getAttribute('data-options')))
 
 Formio.createForm(root, formData, options).then(form => {
   console.log('form ready:', form)
-  document.title = form.schema.title
 })
 
 function tryParse (str) {
