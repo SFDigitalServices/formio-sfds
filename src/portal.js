17a94b11ea4389e75e90ba37e1d6564167b83d9c
Shawn Allen <shawnbot@users.noreply.github.com>
2020-08-24 13:03:49 -0700

    tidy up portal.js

--- src/portal.js
+++ src/portal.js
@@ -4,33 +4,34 @@
  */
 import templates from './templates/custom'
 import components from './components'
+
+// these are "classless" components: templates for existing components
+// that don't need any custom functionality or templates
 import builderComponents from './components/builder'
 
 const { Formio } = window
-
-const options = {
-  builder: {
-    builder: {
-      sfgov: {
-        title: 'SF.gov',
-        weight: -100, // bring it to the top, baby
-        default: true, // open by default
-        components: builderComponents
-      },
-      basic: {
-        default: true // set this to false to collapse it
-      },
-      premium: false
-    }
-  }
-}
 
 const framework = 'sfds'
 
 Formio.use({
   framework,
   components,
-  options,
+  options: {
+    builder: {
+      builder: {
+        sfgov: {
+          title: 'SF.gov',
+          weight: -100, // bring it to the top
+          default: true, // open by default
+          components: builderComponents
+        },
+        basic: {
+          // set this to false to collapse it by default
+          default: true
+        }
+      }
+    }
+  },
   templates: {
     [framework]: templates
   }
