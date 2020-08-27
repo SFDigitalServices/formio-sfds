1aa0dea1a0fae63dfa6077cbe4f4906b7f834356
Shawn Allen <shawnbot@users.noreply.github.com>
2020-08-27 13:55:05 -0700

    nix https:// prefix

--- .github/actions/wait-for-deployment/index.js
+++ .github/actions/wait-for-deployment/index.js
@@ -57,7 +57,7 @@ async function waitForDeployment (options) {
         return {
           deployment,
           status: success,
-          url: `https://${success.target_url}`
+          url: success.target_url
         }
       } else {
         core.info(`No statuses with state === "success": "${statuses.map(status => status.state).join('", "')}"`)
