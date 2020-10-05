06d855ce6a93d2842c80e84337ec3a732dacf49e
Shawn Allen <shawnbot@users.noreply.github.com>
2020-10-05 15:20:46 -0700

    add babel jsx transform

--- babel.config.js
+++ babel.config.js
@@ -4,12 +4,11 @@ module.exports = {
     }]
   ],
   plugins: [
-    ['@babel/plugin-proposal-optional-chaining', {
-    }],
+    '@babel/plugin-transform-react-jsx',
+    '@babel/plugin-proposal-optional-chaining',
     ['@babel/plugin-transform-runtime', {
       regenerator: true
     }],
-    ['babel-plugin-lodash', {
-    }]
+    'babel-plugin-lodash'
   ]
 }
