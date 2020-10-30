634dbc9fe7515af79a7f7fd5a14e48f8c3c81a2c
Shawn Allen <shawnbot@users.noreply.github.com>
2020-10-30 15:56:22 -0700

    better commit metadata again

--- views/data.js
+++ views/data.js
@@ -1,11 +1,6 @@
 const {
   NODE_ENV,
   VERCEL_URL,
-  VERCEL_GITHUB_DEPLOYMENT,
-  VERCEL_GITHUB_ORG,
-  VERCEL_GITHUB_REPO,
-  VERCEL_GITHUB_COMMIT_ORG,
-  VERCEL_GITHUB_COMMIT_REPO,
   VERCEL_GITHUB_COMMIT_REF = 'main',
   VERCEL_GITHUB_COMMIT_SHA,
   VERCEL_GITHUB_COMMIT_AUTHOR_LOGIN,
@@ -17,21 +12,11 @@ module.exports = {
   debug: NODE_ENV === 'test',
   url: VERCEL_URL,
   git: {
-    branch: VERCEL_GITHUB_COMMIT_REF,
-    github: {
-      is: VERCEL_GITHUB_DEPLOYMENT,
-      org: VERCEL_GITHUB_ORG,
-      repo: VERCEL_GITHUB_REPO,
-      commit: {
-        org: VERCEL_GITHUB_COMMIT_ORG,
-        repo: VERCEL_GITHUB_COMMIT_REPO,
-        ref: VERCEL_GITHUB_COMMIT_REF,
-        sha: VERCEL_GITHUB_COMMIT_SHA,
-        author: {
-          login: VERCEL_GITHUB_COMMIT_AUTHOR_LOGIN,
-          name: VERCEL_GITHUB_COMMIT_AUTHOR_NAME
-        }
-      }
+    ref: VERCEL_GITHUB_COMMIT_REF,
+    sha: VERCEL_GITHUB_COMMIT_SHA,
+    author: {
+      login: VERCEL_GITHUB_COMMIT_AUTHOR_LOGIN,
+      name: VERCEL_GITHUB_COMMIT_AUTHOR_NAME
     }
   }
 }
