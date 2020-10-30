1b2ba959af45ac3cb5d1536be4a6870988f9d1ea
Shawn Allen <shawnbot@users.noreply.github.com>
2020-10-30 14:48:09 -0700

    clean up view data, github edit links

--- views/data.js
+++ views/data.js
@@ -1,3 +1,12 @@
+const {
+  VERCEL_GITHUB_COMMIT_REF: branch = 'main'
+} = process.env
+
 module.exports = {
-  pkg: require('../package.json')
+  pkg: require('../package.json'),
+  debug: process.env.NODE_ENV === 'test',
+  env: process.env,
+  git: {
+    branch
+  }
 }
