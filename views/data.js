833710988b6b5c0feb3d38f5408d4180435ed71f
Shawn Allen <shawnbot@users.noreply.github.com>
2020-10-30 15:30:56 -0700

    add more git meta

--- views/data.js
+++ views/data.js
@@ -1,12 +1,36 @@
 const {
-  VERCEL_GITHUB_COMMIT_REF: branch = 'main'
+  NODE_ENV,
+  VERCEL_URL,
+  VERCEL_GITHUB_DEPLOYMENT,
+  VERCEL_GITHUB_ORG,
+  VERCEL_GITHUB_REPO,
+  VERCEL_GITHUB_COMMIT_ORG,
+  VERCEL_GITHUB_COMMIT_REPO,
+  VERCEL_GITHUB_COMMIT_REF = 'main',
+  VERCEL_GITHUB_COMMIT_SHA,
+  VERCEL_GITHUB_COMMIT_AUTHOR_LOGIN,
+  VERCEL_GITHUB_COMMIT_AUTHOR_NAME
 } = process.env
 
 module.exports = {
   pkg: require('../package.json'),
-  debug: process.env.NODE_ENV === 'test',
-  env: process.env,
+  debug: NODE_ENV === 'test',
+  url: VERCEL_URL,
   git: {
-    branch
+    branch: VERCEL_GITHUB_COMMIT_REF,
+    github: VERCEL_GITHUB_DEPLOYMENT ? {
+      org: VERCEL_GITHUB_ORG,
+      repo: VERCEL_GITHUB_REPO,
+      commit: {
+        org: VERCEL_GITHUB_COMMIT_ORG,
+        repo: VERCEL_GITHUB_COMMIT_REPO,
+        ref: VERCEL_GITHUB_COMMIT_REF,
+        sha: VERCEL_GITHUB_COMMIT_SHA,
+        author: {
+          login: VERCEL_GITHUB_COMMIT_AUTHOR_LOGIN,
+          name: VERCEL_GITHUB_COMMIT_AUTHOR_NAME
+        }
+      }
+    } : false
   }
 }
