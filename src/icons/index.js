f4ad3659a6ac8f526b551b60cb25c2563445d866
Anthony Kong <anthony.kong@sfgov.org>
2020-11-24 23:27:29 -0800

    SG-1235 - add icons

--- src/icons/index.js
+++ src/icons/index.js
@@ -3,6 +3,8 @@ import next from './Arrow-right.svg'
 import prev from './Arrow-left.svg'
 import calendar from './Calendar.svg'
 import check from './Check.svg'
+import circleCheck from './Circle-check.svg'
+import chevron from './Chevron.svg'
 import close from './Close.svg'
 import document from './Document.svg'
 import alert from './Alert.svg'
@@ -15,6 +17,8 @@ const icons = {
   add: plus,
   calendar,
   check,
+  chevron,
+  circleCheck,
   close,
   delete: close,
   document,
