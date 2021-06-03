c2ab75c7c7540d7c0fbd2dbcffe7574759e7054f
Shawn Allen <shawnbot@users.noreply.github.com>
2021-06-03 13:00:02 -0700

    Release 8.1.0 (#187)
    * 8.1.0
    
    * ✍️ Signature style  (#186)
    
    * add signature example
    
    * delete old signature styles
    
    * update signature template, import component
    
    * add Signature component overrides
    
    * grey-1 -> grey-2
    
    * fix up "Sign above" text placement

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 import address from './address'
 import review from './review'
 import columns from './columns'
+import signature from './signature'
 import state from './state'
 import zip from './zip'
 
@@ -9,5 +10,6 @@ export default {
   review,
   columns,
   state,
+  signature,
   zip
 }
