044471985e4b2738488b1703b7da80e7937a9024
Shawn Allen <shawnbot@users.noreply.github.com>
2021-09-07 14:29:14 -0700

    delete /api

--- api/index.js
+++
@@ -1,7 +0,0 @@
-const { Router } = require('express')
-
-module.exports = Router()
-  .get('/preview', require('./preview'))
-  .get('/strings', require('./strings'))
-  .get('/translate', require('./translate'))
-  .get('/wtf', require('./wtf'))
