3a9c134b02e3122ebad0e677cafae45d083b6659
Shawn Allen <shawnbot@users.noreply.github.com>
2021-09-07 14:29:46 -0700

    tidy up server.js, lib/sfgov.js

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 const express = require('express')
 
-const { PORT } = process.env
-
 const app = express()
 
 app
-  .use('/api', require('./api'))
   .use('/sfgov', require('./lib/sfgov'))
   /*
    * FIXME: because of how views are built right now, we have to serve /dist
@@ -22,7 +19,7 @@ app
     res.status(404).send(`404: "${req.path}"`)
   })
 
-const server = app.listen(PORT, () => {
+const server = app.listen(process.env.PORT, () => {
   const { address, port } = server.address()
   console.log('server listening at %s:%s', address === '::' ? 'localhost' : address, port)
 })
