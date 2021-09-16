6b47538b5fc995d44c1e5eef0bfbaf23e1e137c7
Shawn Allen <shawnbot@users.noreply.github.com>
2021-09-16 14:02:42 -0700

    use preval macro to strip empty translation entries

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,6 +1,20 @@
-export default {
-  en: require('./en.json'),
-  es: require('./es.json'),
-  tl: require('./tl.json'),
-  zh: require('./zh.json')
-}
+import preval from 'babel-plugin-preval/macro'
+
+const translations = preval`
+  const langs = ['en', 'es', 'tl', 'zh']
+  for (const lang of langs) {
+    module.exports[lang] = removeEmptyEntries(require('./' + lang + '.json'))
+  }
+
+  function removeEmptyEntries (obj) {
+    const copy = {}
+    for (const [key, value] of Object.entries(obj)) {
+      if (value !== '') {
+        copy[key] = value
+      }
+    }
+    return copy
+  }
+`
+
+export default translations
