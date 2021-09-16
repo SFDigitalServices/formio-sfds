847b3599d423ded3f0ae00c402786ef28a711989
Shawn Allen <shawnbot@users.noreply.github.com>
2021-09-16 11:34:48 -0700

    make embedded translations work without key paths

--- src/i18n/load.js
+++ src/i18n/load.js
@@ -1,4 +1,4 @@
-import dot from 'dot-component'
+import dot from 'dotmap'
 import { I18NEXT_DEFAULT_NAMESPACE } from '../constants'
 
 const { FormioUtils } = window
@@ -11,38 +11,45 @@ export function loadEmbeddedTranslations (form) {
   }
 }
 
-export function getEmbeddedTranslations (component) {
+export function getEmbeddedTranslations (form) {
   const bundles = {}
-  FormioUtils.eachComponent(component.components, comp => {
-    const { key, properties } = comp
-    if (properties instanceof Object) {
-      for (const prop in properties) {
-        const value = properties[prop]
-        if (/^[-a-z]+:/i.test(prop)) {
-          const [lang, field] = prop.split(':')
-          const stringId = `${key}.${field}`
+  FormioUtils.eachComponent(form.components, comp => {
+    if (!comp.properties) return
+
+    for (const prop in comp.properties) {
+      const value = comp.properties[prop]
+      if (/^[-a-z]+:/i.test(prop)) {
+        const [lang, path] = prop.split(':')
+        const english = dot.get(comp, path)
+        if (english && typeof english === 'string') {
           if (!bundles[lang]) bundles[lang] = {}
-          bundles[lang][stringId] = value
+          // console.log('%s: %s "%s" -> "%s', lang, path, english, value)
+          bundles[lang][english] = value
+        } else {
+          // console.warn('%s: %s (no english)', lang, path, english)
         }
       }
     }
   }, true)
   if (Object.keys(bundles).length) {
-    console.info('found embedded translations:', bundles)
+    // console.info('found embedded translations:', bundles)
   }
   return bundles
 }
 
 export function addReverseLookups (form, languages = ['es', 'fil', 'zh']) {
   const { i18next } = form
-  const englishStrings = Object.entries(
-    i18next.getResourceBundle('en', I18NEXT_DEFAULT_NAMESPACE)
-  )
+  const englishBundle = i18next.getResourceBundle('en', I18NEXT_DEFAULT_NAMESPACE)
+  if (!englishBundle) {
+    return false
+  }
+
+  const englishStrings = Object.entries(englishBundle)
     .filter(([key, value]) => key !== value)
+
   for (const lang of languages) {
     const current = i18next.getResourceBundle(lang, I18NEXT_DEFAULT_NAMESPACE) || {}
     const bundle = {}
-
     for (const [key, english] of englishStrings) {
       if (typeof english === 'string' && !current[english]) {
         bundle[english] = `$t(${key})`
@@ -50,4 +57,5 @@ export function addReverseLookups (form, languages = ['es', 'fil', 'zh']) {
     }
     i18next.addResourceBundle(lang, I18NEXT_DEFAULT_NAMESPACE, bundle)
   }
+  return true
 }
