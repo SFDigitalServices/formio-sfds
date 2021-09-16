ab5952cb43631cd16c56d95ecf75709c5df15111
Shawn Allen <shawnbot@users.noreply.github.com>
2021-09-16 14:08:17 -0700

    add comments to preval'd translations

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,8 +1,14 @@
 import preval from 'babel-plugin-preval/macro'
 
 const translations = preval`
+  /**
+   * This code is evaluated at build time and results in a smaller browser
+   * bundle. It also prevents empty translations from making it into forms,
+   * since empty strings are treated as valid by default.
+   */
   const langs = ['en', 'es', 'tl', 'zh']
   for (const lang of langs) {
+    // export each language code as an object without empty ("") values
     module.exports[lang] = removeEmptyEntries(require('./' + lang + '.json'))
   }
 
