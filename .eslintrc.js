6ea7bfa66436036f8fe27baa431c9a85b4ed239f
Shawn Allen <shawnbot@users.noreply.github.com>
2022-06-07 12:15:13 -0700

    fix(eslint): move storybook + react bits to root config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,31 @@
 /** @type {import('eslint').ESLint.Options} */
 module.exports = {
-  plugins: ['sfgov'],
-  extends: [
-    'plugin:storybook/recommended',
-    'plugin:sfgov/babel',
-    'plugin:sfgov/recommended'
+  plugins: [
+    'react',
+    'sfgov'
   ],
+  extends: [
+    'plugin:react/recommended',
+    'plugin:storybook/recommended',
+    'plugin:sfgov/recommended',
+    'plugin:sfgov/babel'
+  ],
+  parserOptions: {
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
   rules: {
     'promise/always-return': 0,
     'promise/catch-or-return': 0,
     'promise/no-callback-in-promise': 0,
+    'react/prop-types': ['warn'],
     'import/no-unresolved': [2, {
       ignore: [
         '^../dist',
