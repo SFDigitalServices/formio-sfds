c84c2aef580ee9c7a0c7ed57585147a4657097b9
Shawn Allen <shawnbot@users.noreply.github.com>
2022-06-07 13:38:34 -0700

    feat(storybook): add selectboxes (checkboxes) story

---
+++ src/stories/selectboxes.stories.jsx
@@ -0,0 +1,108 @@
+import React from 'react'
+import { Form } from '../react/form'
+
+export default {
+  title: 'Checkboxes',
+  description: '',
+  argTypes: {
+    label: {
+      name: 'Label',
+      description: 'The field &lt;label&gt;'
+    },
+    values: {
+      name: 'Values',
+      description: 'A list of the available choices',
+      table: {
+        type: { summary: 'Array<{label: \'string\', value: \'string\' }>' }
+      }
+    },
+    required: {
+      name: 'Required',
+      table: {
+        type: { summary: 'Boolean' }
+      }
+    },
+    translations: {
+      name: 'Translations',
+      table: {
+        type: { summary: 'Record<string, Record<string, string>>' }
+      }
+    }
+  }
+}
+
+const Template = ({ label, values, language, translations, required, validate, formOptions, ...rest }) => (
+  <Form
+    components={[
+      {
+        key: 'component',
+        label,
+        type: 'selectboxes',
+        values,
+        validate: {
+          required,
+          ...(validate || {})
+        },
+        ...rest
+      }
+    ]}
+    options={{
+      language,
+      i18n: translations
+    }}
+  />
+)
+
+const defaultValues = {
+  label: 'Which ways?',
+  autocomplete: false,
+  values: [
+    { label: 'Up', value: 'up' },
+    { label: 'Down', value: 'down' },
+    { label: 'Left', value: 'left' },
+    { label: 'Right', value: 'right' }
+  ],
+  translations: {
+    es: {
+      'component.label': '¿Cuales direcciónes?',
+      'component.values.up': 'Arriba',
+      'component.values.down': 'Abajo',
+      'component.values.left': 'Izquierda',
+      'component.values.right': 'Derecho'
+    }
+  }
+}
+
+export const Default = Object.assign(
+  Template.bind({}),
+  {
+    args: {
+      language: 'en',
+      ...defaultValues
+    }
+  }
+)
+
+export const Required = Object.assign(
+  Template.bind({}),
+  {
+    args: {
+      language: 'en',
+      ...defaultValues,
+      required: true,
+      validate: {
+        customMessage: 'Please choose at least one'
+      }
+    }
+  }
+)
+
+export const Translated = Object.assign(
+  Template.bind({}),
+  {
+    args: {
+      language: 'es',
+      ...defaultValues
+    }
+  }
+)
