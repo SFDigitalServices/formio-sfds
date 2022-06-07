ff27c5891b4dd37ac8c98b541ee9b265a22b40c1
Shawn Allen <shawnbot@users.noreply.github.com>
2022-06-07 12:44:26 -0700

    fix(heroku): split webpack chunks, add bundle analyzer

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+
 module.exports = {
   framework: '@storybook/react',
   stories: [
@@ -7,5 +9,26 @@ module.exports = {
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials'
-  ]
+  ],
+  webpackFinal: async (config, { configType }) => {
+    // analyze webpack bundle size. To enable run storybook with `cross-env analyze=true`
+    // not needed, but helps debug the problem :)
+    const shouldAnalyze = process.env.analyze === 'true'
+    if (shouldAnalyze) {
+      config.plugins.push(
+        new BundleAnalyzerPlugin()
+      )
+    }
+
+    // split into more chunks
+    config.optimization = {
+      splitChunks: {
+        chunks: 'all',
+        minSize: 30 * 1024, // 30KB
+        maxSize: 1024 * 1024 // 1MB
+      }
+    }
+
+    return config
+  }
 }
