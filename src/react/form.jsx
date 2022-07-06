92f14824195b1f7f15ed8a1e40524e8fb4617497
Shawn Allen <shawnbot@users.noreply.github.com>
2022-07-06 16:51:55 -0700

    chore: refactor form.jsx to use @formio/react Form

--- src/react/form.jsx
+++ src/react/form.jsx
@@ -1,92 +1,38 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
-import '../../dist/formio-sfds.standalone.js'
+import { Form, Formio, Utils } from '@formio/react'
+// FIXME: we have to import from the built JS because EJS
+import theme, { patch } from '../../dist/formio-sfds.mjs'
 
-const { Formio } = window
+Object.assign(window, {
+  Formio,
+  FormioUtils: Utils
+})
 
-export function Form (props) {
-  const {
-    // components={[...]} is shorthand for dataSource={{ components: [...] }}
-    components,
-    dataSource = { components },
-    options,
-    ...rest
-  } = props
-  const ref = useRef()
-  const form = useForm(ref, dataSource, options)
-  console.log('form:', form)
-  return <div {...rest} ref={ref} />
+Formio.use(theme)
+patch(Formio)
+
+export { Form }
+
+export function SingleComponentForm ({ component, ...rest }) {
+  return <Form form={{
+    type: 'form',
+    components: [component]
+  }} {...rest} />
 }
 
-Form.propTypes = {
-  components: PropTypes.arrayOf(
-    PropTypes.object
-  ),
-  dataSource: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.object
-  ]),
-  options: PropTypes.shape({
-    language: PropTypes.string
-  })
+SingleComponentForm.propTypes = {
+  component: PropTypes.object
 }
 
-export function useForm (
-  /** @type {import('react').Ref<HTMLElement>} */
-  ref,
-  /** @type {string | Object} */
-  dataSource,
-  /** @type {Object} */
-  options = {}
-) {
-  const [
-    /** @type {import('formiojs').Form} */
-    form,
-    setForm
-  ] = useState()
-  const {
-    page,
-    on: handlers,
-    submission,
-    ...restOptions
-  } = options || {}
-  useEffect(() => {
-    if (!form && ref.current) {
-      Formio.createForm(
-        ref.current.appendChild(document.createElement('div')),
-        dataSource,
-        restOptions
-      ).then(form => {
-        if (handlers instanceof Object) {
-          for (const [event, handler] of Object.entries(handlers)) {
-            form.on(event, handler)
-          }
-        }
-        if (submission) {
-          form.submission = submission
-        }
-        setForm(form)
-      })
-    }
-    return () => {
-      if (form) {
-        const { element } = form
-        try {
-          form.destroy()
-        } catch (error) {
-          form.empty()
-        }
-        element.remove()
-        setForm(undefined)
-      }
-    }
-  }, [ref, dataSource, restOptions])
+export function Wizard ({ pages = [], ...rest }) {
+  return <Form form={{
+    type: 'form',
+    display: 'wizard',
+    components: pages
+  }} {...rest} />
+}
 
-  useEffect(() => {
-    if (form && !isNaN(page)) {
-      form.setPage(page)
-    }
-  }, [form, page])
-
-  return form
+Wizard.propTypes = {
+  pages: PropTypes.arrayOf(PropTypes.object)
 }
